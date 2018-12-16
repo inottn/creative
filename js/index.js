e44d78eb3fd912c10cdc452b701e72249d60895c
inottn <inottn@outlook.com>
2018-12-16 15:29:49 +0800

    add: 完成切换至shutters-hide-state状态的逻辑

--- js/index.js
+++ js/index.js
@@ -4,5 +4,10 @@ homeEl.addEventListener('click', function() {
   if (this.classList.contains('initial-state')) {
     this.classList.remove('initial-state')
     this.classList.add('visual-text-wrapper-show-state')
+
+    setTimeout(() => {
+      this.classList.remove('visual-text-wrapper-show-state')
+      this.classList.add('shutters-hide-state')
+    }, 300 + 1600)
   }
 })
